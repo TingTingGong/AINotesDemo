985b787628c026176f1ad4f7e9c50d5d9e2fd267
tingting.gong <tingting.gong@appxy.com>
2026-01-27 15:28:18 +0800

    [创建项目管理]

---
+++ AINotesDemo/AINotesDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AINotesDemoApp.swift
+//  AINotesDemo
+//
+//  Created by 宫廷 on 2026/1/27.
+//
+
+import SwiftUI
+
+@main
+struct AINotesDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AINotesDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AINotesDemo
+//
+//  Created by 宫廷 on 2026/1/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
